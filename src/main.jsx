9ea97000fdcf863020aece5a7cc9f084a5f661b2
Wiktor <polaczekmr@gmail.com>
2024-01-25 16:48:20 +0100

    end

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Board from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Board from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <React.StrictMode>
-      <div className='flex flex-col items-center justify-center w-full h-full mt-96'>
-    <Board />
+    <div className='flex flex-col items-center justify-center w-full h-full mt-96'>
+      <Board />
     </div>
   </React.StrictMode>
-,
-)
+);
